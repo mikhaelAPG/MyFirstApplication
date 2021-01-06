c05d1ec43fa944c3f4ac3cfaecf0c5d10c3a3b23
Mikhael <mikhaelapg00@gmail.com>
2021-01-07 00:45:00 +0700

    explore fragment

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myfirstapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("Main Activity", "onCreate")
+
+        btn_watch.setOnClickListener {
+            val i = Intent(this, MovieDetailActivity::class.java)
+            startActivity(i)
+        }
+
+        btn_fragment2.setOnClickListener {
+            var fr = supportFragmentManager.beginTransaction()
+            fr.replace(R.id.fl_fragment, ExploreFragment())
+            fr.commit()
+        }
+
+        var fr = supportFragmentManager.beginTransaction()
+        fr.add(R.id.fl_fragment, HomeFragment())
+        fr.commit()
+    }
+}
