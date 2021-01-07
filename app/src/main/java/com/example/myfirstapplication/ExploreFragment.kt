7729b394e58f9df3ecafeb03b12dac5e5c33f3c8
Mikhael <mikhaelapg00@gmail.com>
2021-01-07 19:12:25 +0700

    Infinite Scroll Pagination

--- app/src/main/java/com/example/myfirstapplication/ExploreFragment.kt
+++ app/src/main/java/com/example/myfirstapplication/ExploreFragment.kt
@@ -2,11 +2,14 @@ package com.example.myfirstapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfirstapplication.adapter.MovieAdapter
 import com.example.myfirstapplication.listener.OnItemClickListener
 import com.example.myfirstapplication.model.Movie
@@ -17,6 +20,7 @@ class ExploreFragment : Fragment() {
     lateinit var movieAdapter: MovieAdapter
     val lm = LinearLayoutManager(activity)
     val addMoviewList: MutableList<Movie> = ArrayList()
+    var isLoading = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -55,5 +59,38 @@ class ExploreFragment : Fragment() {
                 startActivity(i)
             }
         })
+
+        rv_movie.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+
+                if(dy > 0) {
+                    var vItem = lm.childCount
+                    var lItem = lm.findFirstCompletelyVisibleItemPosition()
+                    var count = movieAdapter.itemCount
+
+                    if (!isLoading) {
+                        if (vItem + lItem >= count) {
+                            addMoreData()
+                        }
+                    }
+                }
+                super.onScrolled(recyclerView, dx, dy)
+            }
+        })
+    }
+
+    fun addMoreData() {
+        isLoading = true
+        pg_bar.visibility = View.VISIBLE
+        for (i in 0..6) {
+            addMoviewList.add(Movie("Ini Movie ke-" + i + "", "Lorem Ipsum Sing Amet Wedus Lara Aporto"))
+        }
+
+        Handler().postDelayed({
+            isLoading = false
+            pg_bar.visibility = View.GONE
+            movieAdapter.setMovie(addMoviewList)
+        }, 5000)
+
     }
 }
