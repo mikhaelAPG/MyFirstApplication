b9cd2fc922fb1068560c043299ce4bb53b470bd5
Mikhael <mikhaelapg00@gmail.com>
2021-01-07 10:13:57 +0700

    bottom navigation

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -10,30 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Log.i("Main Activity", "onCreate")
-
-        btn_watch.setOnClickListener {
-            val i = Intent(this, MovieDetailActivity::class.java)
-            startActivity(i)
-        }
-
-        btn_fragment2.setOnClickListener {
-            var fr = supportFragmentManager.beginTransaction()
-            fr.replace(R.id.fl_fragment, ExploreFragment())
-            fr.commit()
-        }
 
         var fr = supportFragmentManager.beginTransaction()
         fr.add(R.id.fl_fragment, HomeFragment())
-        fr.commit()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        var fr = supportFragmentManager.beginTransaction()
-        fr.replace(R.id.fl_fragment, HomeFragment())
         fr.commit()
     }
 }
